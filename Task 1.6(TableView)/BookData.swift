f8a1b1539a4ea8f2309e3bdfd337487ad110c322
nathan1026 <nathanbahl@gmail.com>
2016-07-06 08:54:11 -0700

    Initial Commit

---
+++ Task 1.6(TableView)/BookData.swift
@@ -0,0 +1,11 @@
+//
+//  BookData.swift
+//  Task 1.6(TableView)
+//
+//  Created by Nathan Bahl on 7/6/16.
+//  Copyright © 2016 Nathan Bahl. All rights reserved.
+//
+
+import UIKit
+
+var bookArray = ["A Million Little Pieces", "All the Light We Cannot See", "Room", "Maus", "Blankets"]
